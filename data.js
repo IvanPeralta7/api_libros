8e19ee721df3898041a109c3aaeb8dce5ed60d53
Ivan peralta <ivanperalta_cba2013@live.com>
2023-07-22 11:57:18 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const libros = [
+    {id: '1', titulo: 'Orgullo y Prejuicio', autor: 'Jane Austen'},
+    {id: '2', titulo: 'Divina comedia', autor: 'Dante Alighieri'},
+    {id: '3', titulo: 'Cien años de soledad', autor: 'Gabriel Garcia Marques'},
+];
+
+module.exports = libros;
